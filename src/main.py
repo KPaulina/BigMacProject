b1d28601d8fa5ce4ee067808aa1626729c379ae3
KPaulina <41211513+KPaulina@users.noreply.github.com>
2023-02-02 22:14:17 +0100

    fix: updating requirements.txt

--- src/main.py
+++ src/main.py
@@ -9,3 +9,4 @@ if __name__ == "__main__":
     df_big_mac_values_for_all_countries = scrape_api_for_big_mac_data(country_codes_list)
     df_big_mac_values_for_all_countries.to_excel(os.path.join(DATA_DIR, 'big_mac_data.xlsx'), index=False)
     upload_file(os.path.join(DATA_DIR, 'big_mac_data.xlsx'), 'bucketbigmac', 'big_mac_data.xlsx')
+
